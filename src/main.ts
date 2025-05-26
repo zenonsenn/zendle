a20547fed081c81eb55e75269f5037b5d425e480
hugoalfedo <zenonsenn@gmail.com>
2025-05-26 13:42:16 +0700

    Boilerplate

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './assets/output.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
